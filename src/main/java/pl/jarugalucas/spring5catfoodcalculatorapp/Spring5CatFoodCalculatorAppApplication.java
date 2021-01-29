fb51605942c83ab3136addfbdded7baa59c01f79
Lucas Jaruga <lucas.jaruga@outlook.com>
2021-01-29 14:20:47 +0100

    beginning of the project

---
+++ src/main/java/pl/jarugalucas/spring5catfoodcalculatorapp/Spring5CatFoodCalculatorAppApplication.java
@@ -0,0 +1,13 @@
+package pl.jarugalucas.spring5catfoodcalculatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5CatFoodCalculatorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5CatFoodCalculatorAppApplication.class, args);
+    }
+
+}
